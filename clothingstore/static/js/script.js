99db628482d58d6bef89327cb750ff441e82c19c
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-02-15 17:37:44 +0530

    Login and Logout Views

--- clothingstore/static/js/script.js
+++ clothingstore/static/js/script.js
@@ -58,10 +58,10 @@ $(document).ready(function() {
 }); 
 // jquery end
 
-// setTimeout(function(){
-//     $("#message").fadeOut('slow')
-// }, 4000)
+setTimeout(function(){
+    $("#message").fadeOut('slow')
+}, 4000)
 
-setTimeout(function() {
-    $(".alert").fadeOut('slow');
-}, 4000);
+// setTimeout(function(){
+//     $(".alert").fadeOut('slow');
+// }, 4000);
