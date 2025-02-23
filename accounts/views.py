de5f58a3afb1fde297451274d794e28f96e58bf5
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-02-23 13:15:50 +0530

    Dashboard Page

--- accounts/views.py
+++ accounts/views.py
@@ -16,6 +16,7 @@ from django.core.mail import EmailMessage
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
 import requests
+from orders.models import Order
 
 def register(request):
     if request.method == 'POST':
@@ -145,7 +146,12 @@ def activate(request, uidb64, token):
 
 @login_required(login_url="login")
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+    context = {
+        'orders_count': orders_count,
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def forgotPassword(request):
